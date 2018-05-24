a880591a0c02435ab2c1d88849b3bdb7013514c2
Mitch Lembke <mitchell.lembke@gmail.com>
2018-05-24 15:05:58 -0600

    added reducer

---
+++ src/reducers/auth-reducer.js
@@ -0,0 +1,17 @@
+const AuthReducer = (state = null, action) => {
+  const initialState = {
+    username: null,
+    token: null,
+    error: null
+  }
+  if(state) {
+    switch(action.type) {
+      default:
+        return state
+    }
+  } else {
+    return {...initialState}
+  }
+}
+
+export default AuthReducer
