96224116a7e2f3150c566ea2bcc943ada4f257e7
Mitch Lembke <mitchell.lembke@gmail.com>
2018-05-24 15:34:11 -0600

    maping state to props in component

--- src/components/header/header-container.js
+++ src/components/header/header-container.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import './header-container.css'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'react-redux'
 
-const Header = () => (
-  <h1 className="header-container">Header</h1>
-)
+class Header extends React.Component {
 
-export default Header
+  onLogoutClick = event => {
+    event.preventDefault()
+    this.props.logout()
+  }
+    render(){
+      return(
+        <div className="header-conatiner">
+          <a className='title' href="#title">Stupid Todos</a>
+        { this.props.username !== null && <a href='#logout' onClick={ this.onLogoutClick }>{ `Logout ${ this.props.username }` }</a>}
+        </div>
+      )
+    }
+}
+
+const mapStateToProps = state => state.auth
+
+// const mapDispatchToProps = dispatch => bindActionCreators({logout}, dispatch)
+
+export default connect(mapStateToProps)(Header)
