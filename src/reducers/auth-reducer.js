47e89928de51d1a451d5fb05011e6dc07bca34ce
Mitch Lembke <mitchell.lembke@gmail.com>
2018-05-24 16:36:25 -0600

    login works

--- src/reducers/auth-reducer.js
+++ src/reducers/auth-reducer.js
@@ -1,3 +1,5 @@
+import { LOGIN_FAIL, LOGIN_PASS } from '../actions/auth-actions'
+
 const AuthReducer = (state = null, action) => {
   const initialState = {
     username: null,
@@ -6,6 +8,21 @@ const AuthReducer = (state = null, action) => {
   }
   if(state) {
     switch(action.type) {
+      case LOGIN_PASS:
+      debugger
+        return {
+          ...state,
+          username: action.payload.username,
+          token: action.payload.token,
+          error: null
+        }
+      case LOGIN_FAIL:
+          return {
+            ...state,
+            username: null,
+            token: null,
+            error: action.payload.error
+          }
       default:
         return state
     }
